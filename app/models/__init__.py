8779fe73046755643e8a7feb572880615e24e365
vaibhav9979 <128869624+vaibhav9979@users.noreply.github.com>
2025-12-29 04:46:46 +0530

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user import User
-from .vehicle import Vehicle
-from .station import FuelStation, StationEmployee
-from .compliance import ComplianceRecord
-from .document import Document
-from .qr_code import QRCode
-from .notification import Notification
-from .rating import StationRating
-from .security_log import SecurityLog
