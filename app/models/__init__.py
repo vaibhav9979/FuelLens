2ac2d03ce080ea7524ce9bef3b857e1f4c05af3e
vaibhav9979 <128869624+vaibhav9979@users.noreply.github.com>
2025-12-29 05:38:57 +0530

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Import models after db is defined
 from .user import User
+from .station import Station
 from .vehicle import Vehicle
-from .station import FuelStation, StationEmployee
-from .compliance import ComplianceRecord
+from .compliance import Compliance
 from .document import Document
-from .qr_code import QRCode
 from .notification import Notification
-from .rating import StationRating
+from .qr_code import QRCode
+from .rating import Rating
 from .security_log import SecurityLog
+
+# Export models
+__all__ = ['db', 'User', 'Station', 'Vehicle', 'Compliance', 'Document', 'Notification', 'QRCode', 'Rating', 'SecurityLog']
